94a8e4d5eab36324b335a78ac68bd663ec497799
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-02-08 12:39:42 +0100

    refactor: make use of expect_named

--- tests/testthat/test-rgp.R
+++ tests/testthat/test-rgp.R
@@ -1,12 +1,13 @@
 test_that("user defined covariance works", {
-  my_sqexp <- function(s, t) exp( -(s - t)^2 / 0.1)
+  my_sqexp <- function(s, t) exp(-(s - t)^2 / 0.1)
   set.seed(1312)
   x1 <- tf_rgp(1, cov = my_sqexp)
   set.seed(1312)
   x2 <- tf_rgp(1, cov = "squareexp")
   expect_equal(x1, x2)
-  
-  expect_error(tf_rgp(1, cov = function(s, t, x) (s-t)/x),
-               "2 formal arguments")
+
+  expect_error(
+    tf_rgp(1, cov = function(s, t, x) (s - t) / x),
+    "2 formal arguments"
+  )
 })
-  
