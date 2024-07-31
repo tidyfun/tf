3a2d6d6e517ac84f306bd3f6fee6580fd65bc11e
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-07-31 10:11:17 +0200

    tests: NA case

--- tests/testthat/test-tfd-class.R
+++ tests/testthat/test-tfd-class.R
@@ -31,4 +31,16 @@ test_that("tfd.numeric works", {
   expect_type(attr(f, "evaluator"), "closure")
   expect_named(formals(attr(f, "evaluator")), c("x", "arg", "evaluations"))
   expect_identical(attr(f, "evaluator_name"), "tf_approx_linear")
+
+  # single NA
+  for (x in list(NA_real_, NA_integer_)) {
+    f <- tfd(x)
+    expect_s3_class(f, "tfd_irreg")
+    expect_length(f, 0)
+    expect_identical(attr(f, "arg"), list(1L))
+    expect_identical(attr(f, "domain"), c(0, 0))
+    expect_type(attr(f, "evaluator"), "closure")
+    expect_named(formals(attr(f, "evaluator")), c("x", "arg", "evaluations"))
+    expect_identical(attr(f, "evaluator_name"), "tf_approx_linear")
+  }
 })
