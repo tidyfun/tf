bde2ef6f3f34589b0c85dd0edcc4eaa13111109b
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-08-01 10:54:54 +0200

    tests: more test for matrix conversion

--- tests/testthat/test-convert.R
+++ tests/testthat/test-convert.R
@@ -47,7 +47,17 @@ test_that("as.matrix.tf works", {
   set.seed(1312)
   x <- tf_rgp(3)
   mat <- as.matrix(x)
-  expect_matrix(mat)
+  expect_matrix(mat, mode = "numeric", nrows = 3, ncols = 51)
+  expect_identical(row.names(mat), as.character(1:3))
+  expect_identical(colnames(mat), as.character(tf_arg(x)))
+  expect_identical(attr(mat, "arg"), tf_arg(x))
+
+  arg <- seq(0, 1, by = 0.04)
+  mat <- as.matrix(x, arg = arg)
+  expect_matrix(mat, mode = "numeric", nrows = 3, ncols = 26)
+  expect_identical(row.names(mat), as.character(1:3))
+  expect_identical(colnames(mat), as.character(arg))
+  expect_identical(attr(mat, "arg"), arg)
 })
 
 test_that("as.function.tf works", {
