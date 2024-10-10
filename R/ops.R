0093631f9ce1d318b6870679642710421ab2da7c
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-10-10 14:35:55 +0200

    refactor: migrate to vec_arith API

--- R/ops.R
+++ R/ops.R
@@ -51,156 +51,126 @@ fun_op <- function(x, y, op, numeric = NA) {
   ret
 }
 
-#' Math, Summary and Ops Methods for `tf`
-#'
-#' These methods and operators mostly work `arg`-value-wise on `tf` objects, see
-#' `?groupGeneric` for implementation details.
-#'
-#' See examples below. Equality checks of functional objects are even more iffy
-#' than usual for computer math and not very reliable. Note that `max` and `min`
-#' are not guaranteed to be maximal/minimal over the entire domain, only on the
-#' evaluation grid used for computation. With the exception of addition and
-#' multiplication, operations on `tfb`-objects first evaluate the data on their
-#' `arg`, perform computations on these evaluations and then convert back to an
-#' `tfb`- object, so a loss of precision should be expected -- especially so for
-#' small spline bases and/or very wiggly data.
-#'
-#'
-#' @param x an `tf`
-#' @param ... `tf`-objects (not used for `Math` group generic)
-#' @param e1 an `tf` or a numeric vector
-#' @param e2 an `tf` or a numeric vector
-#' @returns a `tf`- or `logical` vector with the computed result
-#' @seealso [tf_fwise()] for scalar summaries of each function in a `tf`-vector
-#' @examples
-#' set.seed(1859)
-#' f <- tf_rgp(4)
-#' 2 * f == f + f
-#' sum(f) == f[1] + f[2] + f[3] + f[4]
-#' log(exp(f)) == f
-#' plot(f, points = FALSE)
-#' lines(range(f), col = 2, lty = 2)
-#'
-#' f2 <- tf_rgp(5) |> exp() |> tfb(k = 25)
-#' layout(t(1:3))
-#' plot(f2, col = gray.colors(5))
-#' plot(cummin(f2), col = gray.colors(5))
-#' plot(cumsum(f2), col = gray.colors(5))
-#'
-#' # ?tf_integrate for integrals, ?tf_fwise for scalar summaries of each function
-#' @rdname tfgroupgenerics
 #' @export
-#' @family tidyfun compute functions
-Ops.tf <- function(e1, e2) {
-  not_defined <- switch(.Generic,
-    `%%` = ,
-    `%/%` = ,
-    `&` = ,
-    `|` = ,
-    `!` = ,
-    `<` = ,
-    `<=` = ,
-    `>=` = ,
-    `>` = TRUE,
-    FALSE
+#' @method vec_arith tfd
+vec_arith.tfd <- function(op, x, y, ...) {
+  UseMethod("vec_arith.tfd", y)
+}
+
+#' @export
+vec_arith.tfd.default <- function(op, x, y, ...) {
+  stop_incompatible_op(op, x, y)
+}
+
+#' @export
+#' @method vec_arith.tfd tfd
+vec_arith.tfd.tfd <- function(op, x, y, ...) {
+  switch(op,
+    `+` = ,
+    `-` = ,
+    `*` = ,
+    `/` = fun_op(x, y, op),
+    stop_incompatible_op(op, x, y)
   )
-  if (not_defined) {
-    stop(sprintf("%s not defined for \"tf\" objects", .Generic), call. = FALSE)
-  }
-  if (nargs() == 1) {
-    return(fun_op(0, e1, .Generic, numeric = 1))
-  }
 }
 
-#' @rdname tfgroupgenerics
 #' @export
-`==.tfd` <- function(e1, e2) {
-  # no "recycling" of args
-  stopifnot(
-    length(e2) %in% c(1, length(e1)) || length(e1) %in% c(1, length(e2))
+#' @method vec_arith.tfd numeric
+vec_arith.tfd.numeric <- function(op, x, y, ...) {
+  switch(op,
+    `+` = ,
+    `-` = ,
+    `/` = ,
+    `*` = ,
+    `^` = fun_op(x, y, op, numeric = 2),
+    stop_incompatible_op(op, x, y)
   )
-  # not comparing names, as per convention...
-  same <- all(compare_tf_attribs(e1, e2))
-  if (!same) {
-    return(rep(FALSE, max(length(e1), length(e2))))
-  }
-  map2_lgl(e1, e2, \(x, y) isTRUE(all.equal(x, y)))
 }
 
-#' @rdname tfgroupgenerics
 #' @export
-`!=.tfd` <- function(e1, e2) !(e1 == e2)
-
-# need to copy instead of defining tf-method s.t. dispatch in Ops works
-#' @rdname tfgroupgenerics
-#' @export
-`==.tfb` <- eval(`==.tfd`)
-
-#' @rdname tfgroupgenerics
-#' @export
-`!=.tfb` <- eval(`!=.tfd`)
-
-#' @rdname tfgroupgenerics
-#' @export
-Ops.tfd <- function(e1, e2) {
-  ret <- NextMethod()
-  if (nargs() != 1) {
-    if (is_tfd(e1) && is_tfd(e2)) {
-      if (.Generic == "^") {
-        stop("^ not defined for \"tfd\" objects", call. = FALSE)
-      }
-      return(fun_op(e1, e2, .Generic))
-    }
-    if (is.logical(e1)) e1 <- as.numeric(e1)
-    if (is.logical(e2)) e2 <- as.numeric(e2)
-    if (is_tfd(e1) && is.numeric(e2)) {
-      return(fun_op(e1, e2, .Generic, numeric = 2))
-    }
-    if (is_tfd(e2) && is.numeric(e1)) {
-      return(fun_op(e1, e2, .Generic, numeric = 1))
-    }
-    stop(sprintf(
-      "binary %s not defined for classes %s and %s", .Generic, class(e1)[1], class(e2)[1]
-    ), call. = FALSE)
-  }
-  ret
+#' @method vec_arith.numeric tfd
+vec_arith.numeric.tfd <- function(op, x, y, ...) {
+  switch(op,
+    `+` = ,
+    `-` = ,
+    `/` = ,
+    `*` = fun_op(x, y, op, numeric = 1),
+    stop_incompatible_op(op, x, y)
+  )
 }
 
-#' @rdname tfgroupgenerics
 #' @export
-Ops.tfb <- function(e1, e2) {
-  ret <- NextMethod()
-  if (nargs() != 1) {
-    both_funs <- is_tfb(e1) & is_tfb(e2)
-    if (both_funs) {
-      if (.Generic == "^") {
-        stop("^ not defined for \"tfb\" objects", call. = FALSE)
-      }
-      stopifnot(all(compare_tf_attribs(e1, e2)))
-    }
-    if (both_funs && .Generic %in% c("+", "-")) {
-      # just add/subtract coefs for identical bases
-      return(fun_op(e1, e2, .Generic))
-    }
-    # ... else convert to tfd, compute, refit basis
-    if (both_funs) {
-      basis_args <- attr(e1, "basis_args")
-      eval <- fun_op(tfd(e1), tfd(e2), .Generic)
-    }
-    if (is.logical(e1)) e1 <- as.numeric(e1)
-    if (is.logical(e2)) e2 <- as.numeric(e2)
-    if (is_tfb(e1) && is.numeric(e2)) {
-      basis_args <- attr(e1, "basis_args")
-      eval <- fun_op(tfd(e1), e2, .Generic, numeric = 2)
-    }
-    if (is_tfb(e2) && is.numeric(e1)) {
-      basis_args <- attr(e2, "basis_args")
-      eval <- fun_op(e1, tfd(e2), .Generic, numeric = 1)
-    }
-    return(do.call(
-      "tfb",
-      c(list(eval), basis_args, penalized = FALSE, verbose = FALSE)
-    ))
-  }
-  ret
+#' @method vec_arith.tfd MISSING
+vec_arith.tfd.MISSING <- function(op, x, y, ...) {
+  switch(op,
+    `-` = x * -1,
+    `+` = x,
+    stop_incompatible_op(op, x, y)
+  )
+}
+
+#' @export
+#' @method vec_arith tfb
+vec_arith.tfb <- function(op, x, y, ...) {
+  UseMethod("vec_arith.tfb", y)
+}
+
+#' @export
+vec_arith.tfb.default <- function(op, x, y, ...) {
+  stop_incompatible_op(op, x, y)
+}
+
+#' @export
+#' @method vec_arith.tfb tfb
+vec_arith.tfb.tfb <- function(op, x, y, ...) {
+  stopifnot(all(compare_tf_attribs(x, y)))
+  switch(op,
+    `+` = ,
+    `-` = fun_op(x, y, op),
+    `*` = ,
+    `/` = {
+      basis_args <- attr(x, "basis_args")
+      eval <- fun_op(tfd(x), tfd(y), op)
+      do.call(
+        "tfb",
+        c(list(eval), basis_args, penalized = FALSE, verbose = FALSE)
+      )
+    },
+    stop_incompatible_op(op, x, y)
+  )
+}
+
+#' @export
+#' @method vec_arith.tfb numeric
+vec_arith.tfb.numeric <- function(op, x, y, ...) {
+  switch(op,
+    `+` = ,
+    `-` = ,
+    `/` = ,
+    `*` = ,
+    `^` =  fun_op(x, y, op, numeric = 2),
+    stop_incompatible_op(op, x, y)
+  )
+}
+
+#' @export
+#' @method vec_arith.numeric tfb
+vec_arith.numeric.tfb <- function(op, x, y, ...) {
+  switch(op,
+    `+` = ,
+    `-` = ,
+    `/` = ,
+    `*` = fun_op(x, y, op, numeric = 1),
+    stop_incompatible_op(op, x, y)
+  )
+}
+
+#' @export
+#' @method vec_arith.tfb MISSING
+vec_arith.tfb.MISSING <- function(op, x, y, ...) {
+  switch(op,
+    `-` = x * -1,
+    `+` = x,
+    stop_incompatible_op(op, x, y)
+  )
 }
