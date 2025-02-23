daff7f3039ddcfb82f558f3a715ecb064ab55e2a
Maximilian Muecke <muecke.maximilian@gmail.com>
2025-02-23 11:24:02 +0100

    refactor: use actual names for onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 # dynamically xports format_glimpse.tf if {pillar} is available
 # (see ?s3_register)
 # nocov start
-.onLoad <- function(lib, pkg) {
+.onLoad <- function(libname, pkgname) {
   vctrs::s3_register("pillar::format_glimpse", "tf")
 
   invisible()
