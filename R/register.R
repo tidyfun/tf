aea9f3877708f65c48eaa225905923ae1a3fbf67
Maximilian Muecke <muecke.maximilian@gmail.com>
2025-06-27 14:18:57 +0200

    feat: add wip rgam

--- R/register.R
+++ R/register.R
@@ -265,3 +265,13 @@ tf_register_fda <- function(x, template, ...) {
   warp <- lwr + (upr - lwr) * warp / (matrix(1, nrow = n) %*% warp[n, ])
   tfd(t(warp), arg = arg)
 }
+
+tf_rgam <- function(n, arg = 51L, sigma = 0.1) {
+  rlang::check_installed("fdasrvf")
+  n <- assert_count(n, positive = TRUE, coerce = TRUE)
+  arg <- assert_count(arg, positive = TRUE, coerce = TRUE)
+  assert_number(sigma)
+
+  x <- fdasrvf::rgam(N = arg, sigma = sigma, num = n)
+  tfd(x, arg = seq_len(arg))
+}
