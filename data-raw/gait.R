28bef071f1e9ccb2486b9f1e866426ce78d16e14
Maximilian Muecke <muecke.maximilian@gmail.com>
2025-06-30 23:01:51 +0200

    docs: add dataset documentation

--- data-raw/gait.R
+++ data-raw/gait.R
@@ -1,11 +1,11 @@
 gait <- datasets::gait
 knee_angle <- gait[,, "Knee Angle"]
-knee_angle <- tfd(t(knee_angle))
+knee_angle <- tf::tfd(t(knee_angle))
 hip_angle <- gait[,, "Hip Angle"]
-hip_angle <- tfd(t(hip_angle))
+hip_angle <- tf::tfd(t(hip_angle))
 stopifnot(length(knee_angle) == length(hip_angle))
 gait <- vctrs::data_frame(
-  subject = seq_len(length(knee_angle)),
+  subject_id = seq_along(knee_angle),
   knee_angle = knee_angle,
   hip_angle = hip_angle
 )
